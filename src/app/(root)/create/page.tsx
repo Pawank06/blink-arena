5e487a9ef7040660b951e4a893ab10ff8bcaa502
PawanK06 <pawan06kumar2003@gmail.com>
2024-09-17 21:48:33 +0530

    create cards

--- src/app/(root)/create/page.tsx
+++ src/app/(root)/create/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { TournamentCard } from "@/components/turnament-card";
 import React, { useState } from "react";
 
 interface TournamentFormData {
@@ -79,157 +80,164 @@ const TournamentForm: React.FC = () => {
   };
 
   return (
-    <div className="max-w-2xl mx-auto mt-10 p-6 bg-black shadow-md rounded-md text-cyan-50">
-      <h1 className="text-2xl font-bold mb-6 text-center">Create Tournament</h1>
-      <form onSubmit={handleSubmit} className="space-y-6">
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold ">Organization Name</label>
-          <input
-            type="text"
-            name="organizationName"
-            value={formData.organizationName}
-            onChange={handleInputChange}
-            className="p-3 rounded-md bg-stone-900 border border-stone-800"
-            placeholder="Enter organization name"
-            required
-          />
-        </div>
+    <div className="grid grid-cols-2 place-items-center mx-auto gap-20 mt-10 p-6 bg-black shadow-md rounded-md text-cyan-50 max-w-[1200px]">
+  <div className="w-full max-w-[600px]">
+    <h1 className="text-2xl font-bold mb-6 text-center">Create Tournament</h1>
+    <form onSubmit={handleSubmit} className="space-y-6">
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Organization Name</label>
+        <input
+          type="text"
+          name="organizationName"
+          value={formData.organizationName}
+          onChange={handleInputChange}
+          className="p-3 rounded-md bg-stone-900 border border-stone-800"
+          placeholder="Enter organization name"
+          required
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            className="p-3 rounded-md bg-stone-900 border border-stone-800"
-            placeholder="Enter email"
-            required
-          />
-        </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          className="p-3 rounded-md bg-stone-900 border border-stone-800"
+          placeholder="Enter email"
+          required
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold">Image URL</label>
-          <input
-            type="text"
-            name="image"
-            value={formData.image}
-            onChange={handleInputChange}
-            className="p-3 rounded-md bg-stone-900 border border-stone-800"
-            placeholder="Enter image URL"
-            required
-          />
-        </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Image URL</label>
+        <input
+          type="text"
+          name="image"
+          value={formData.image}
+          onChange={handleInputChange}
+          className="p-3 rounded-md bg-stone-900 border border-stone-800"
+          placeholder="Enter image URL"
+          required
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold">Description</label>
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={handleInputChange}
-            className="p-3 rounded-md bg-stone-900 border border-stone-800"
-            placeholder="Enter description"
-            required
-          />
-        </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Description</label>
+        <textarea
+          name="description"
+          value={formData.description}
+          onChange={handleInputChange}
+          className="p-3 rounded-md bg-stone-900 border border-stone-800"
+          placeholder="Enter description"
+          required
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold">Prize Pool (optional)</label>
-          <input
-            type="text"
-            name="prizePool"
-            value={formData.prizePool}
-            onChange={handleInputChange}
-            className="p-3 bg-stone-900 border border-stone-800 rounded-md"
-            placeholder="Enter Prize Pool"
-          />
-        </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Prize Pool (optional)</label>
+        <input
+          type="text"
+          name="prizePool"
+          value={formData.prizePool}
+          onChange={handleInputChange}
+          className="p-3 bg-stone-900 border border-stone-800 rounded-md"
+          placeholder="Enter Prize Pool"
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold">Date</label>
-          <input
-            type="date"
-            name="date"
-            value={formData.date}
-            onChange={handleInputChange}
-            className="p-3 rounded-md bg-stone-900 border border-stone-800"
-            required
-          />
-        </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Date</label>
+        <input
+          type="date"
+          name="date"
+          value={formData.date}
+          onChange={handleInputChange}
+          className="p-3 rounded-md bg-stone-900 border border-stone-800"
+          required
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold ">Time</label>
-          <input
-            type="time"
-            name="time"
-            value={formData.time}
-            onChange={handleInputChange}
-            className="p-3 bg-stone-900 border border-stone-800 rounded-md"
-            required
-          />
-        </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Time</label>
+        <input
+          type="time"
+          name="time"
+          value={formData.time}
+          onChange={handleInputChange}
+          className="p-3 bg-stone-900 border border-stone-800 rounded-md"
+          required
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold">Location</label>
-          <input
-            type="text"
-            name="location"
-            value={formData.location}
-            onChange={handleInputChange}
-            className="p-3 bg-stone-900 border border-stone-800 rounded-md"
-            required
-            placeholder="Enter Tournament Location"
-          />
-        </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Location</label>
+        <input
+          type="text"
+          name="location"
+          value={formData.location}
+          onChange={handleInputChange}
+          className="p-3 bg-stone-900 border border-stone-800 rounded-md"
+          required
+          placeholder="Enter Tournament Location"
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold">Total Team Members</label>
-          <input
-            type="number"
-            name="totalTeamMembers"
-            value={formData.totalTeamMembers}
-            onChange={handleInputChange}
-            className="p-3 bg-stone-900 border border-stone-800 rounded-md"
-            required
-          />
-        </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Total Team Members</label>
+        <input
+          type="number"
+          name="totalTeamMembers"
+          value={formData.totalTeamMembers}
+          onChange={handleInputChange}
+          className="p-3 bg-stone-900 border border-stone-800 rounded-md"
+          required
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold">Join Fees</label>
-          <input
-            type="number"
-            name="joinFees"
-            value={formData.joinFees}
-            onChange={handleInputChange}
-            className="p-3 bg-stone-900 border border-stone-800 rounded-md"
-            required
-          />
-        </div>
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Join Fees</label>
+        <input
+          type="number"
+          name="joinFees"
+          value={formData.joinFees}
+          onChange={handleInputChange}
+          className="p-3 bg-stone-900 border border-stone-800 rounded-md"
+          required
+        />
+      </div>
 
-        <div className="flex flex-col">
-          <label className="mb-2 font-semibold">Join Fees Type</label>
-          <select
-            name="joinFeesType"
-            value={formData.joinFeesType}
-            onChange={handleInputChange}
-            className="p-3 bg-stone-900 border border-stone-800 rounded-md"
-            required
-          >
-            <option value="">Select</option>
-            <option value="USD">USD</option>
-            <option value="EUR">EUR</option>
-            <option value="INR">INR</option>
-          </select>
-        </div>
-
-        <button
-          type="submit"
-          className="w-full p-3 bg-white text-black font-semibold rounded-md hover:bg-white/90 transition-colors"
+      <div className="flex flex-col">
+        <label className="mb-2 font-semibold">Join Fees Type</label>
+        <select
+          name="joinFeesType"
+          value={formData.joinFeesType}
+          onChange={handleInputChange}
+          className="p-3 bg-stone-900 border border-stone-800 rounded-md"
+          required
         >
-          Create Tournament
-        </button>
-      </form>
-    </div>
+          <option value="">Select</option>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="INR">INR</option>
+        </select>
+      </div>
+
+      <button
+        type="submit"
+        className="w-full p-3 bg-green-500 text-black font-semibold rounded-md hover:bg-green-500/90 transition-colors"
+      >
+        Create Tournament
+      </button>
+    </form>
+  </div>
+  <div className="w-full max-w-[600px] absolute top-16 right-20 md:hidden lg:block">
+    <h1 className=" text-2xl font-bold mb-6 text-start">Blink Preview</h1>
+    <TournamentCard />
+  </div>
+</div>
+
   );
 };
 
