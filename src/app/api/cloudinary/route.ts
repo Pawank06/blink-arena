8c5e174504165e622ae35f431a9b91c8c13c783b
AvhiMaz <avhimazumder5@outlook.com>
2024-10-04 14:00:17 +0530

    fix cloudinary issue

--- src/app/api/cloudinary/route.ts
+++ src/app/api/cloudinary/route.ts
@@ -1,11 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
-  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 
 export const config = {
   api: {
