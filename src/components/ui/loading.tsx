dc29cd08dacd5c288105b87ed2986b8b30432aba
AvhiMaz <avhimazumder5@outlook.com>
2024-10-16 16:29:20 +0530

    add loading component

---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const LoadingScreen: React.FC = () => {
+  return (
+  <div className="custom-font loading-container">
+      <div className="loading-text">
+        <span>L</span>
+        <span>O</span>
+        <span>A</span>
+        <span>D</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
